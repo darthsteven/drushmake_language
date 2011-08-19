8f7e1727e5d01b3bd96d56982be907be9a9a3ece
Steven Jones <steven.jones@computerminds.co.uk>
2011-08-19 21:29:07 +0100

    Setup language

---
+++ components/kodrushmake_UDL_Language.py
@@ -0,0 +1,47 @@
+# Komodo drushmake language service.
+
+import logging
+from koUDLLanguageBase import KoUDLLanguage
+
+
+log = logging.getLogger("kodrushmakeLanguage")
+#log.setLevel(logging.DEBUG)
+
+
+def registerLanguage(registry):
+    log.debug("Registering language drushmake")
+    registry.registerLanguage(KodrushmakeLanguage())
+
+
+class KodrushmakeLanguage(KoUDLLanguage):
+    name = "drushmake"
+    lexresLangName = "drushmake"
+    _reg_desc_ = "%s Language" % name
+    _reg_contractid_ = "@activestate.com/koLanguage?language=%s;1" % name
+    _reg_clsid_ = "7e62ac71-7f25-424f-b4d4-35fe4b735c13"
+    
+
+    #TODO: Update 'lang_from_udl_family' as appropriate for your
+    #      lexer definition. There are four UDL language families:
+    #           M (markup), i.e. HTML or XML
+    #           CSL (client-side language), e.g. JavaScript
+    #           SSL (server-side language), e.g. Perl, PHP, Python
+    #           TPL (template language), e.g. RHTML, Django, Smarty
+    #      'lang_from_udl_family' maps each UDL family code (M,
+    #      CSL, ...) to the sub-language name in your language.
+    #      Some examples:
+    #        lang_from_udl_family = {   # A PHP file can contain
+    #           'M': 'HTML',            #   HTML
+    #           'SSL': 'PHP',           #   PHP
+    #           'CSL': 'JavaScript',    #   JavaScript
+    #        }
+    #        lang_from_udl_family = {   # An RHTML file can contain
+    #           'M': 'HTML',            #   HTML
+    #           'SSL': 'Ruby',          #   Ruby
+    #           'CSL': 'JavaScript',    #   JavaScript
+    #           'TPL': 'RHTML',         #   RHTML template code
+    #        }
+    #        lang_from_udl_family = {   # A plain XML can just contain
+    #           'M': 'XML',             #   XML
+    #        }
+    lang_from_udl_family = {}
